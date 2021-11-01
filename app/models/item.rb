d86865dd5582cbcfd8823956d13061379e79e844
saikumarysk <y.saikumar1997@gmail.com>
2021-10-31 18:36:58 -0700

    Added item_id key which is required for Order

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ApplicationRecord
-	has_many :orders
 	validates :name, presence: true, length: {maximum: 100}, uniqueness: true
 	validates :description, presence: true, length: {maximum: 250}
 	validates :price, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_203924) do
+ActiveRecord::Schema.define(version: 2021_11_01_013117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2021_10_31_203924) do
     t.boolean "is_rated"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "item_id"
   end
 
   create_table "ratings", force: :cascade do |t|
