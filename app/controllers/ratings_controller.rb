266d08c328da7d471968efdce30da853e3bc62c6
saikumarysk <y.saikumar1997@gmail.com>
2021-11-01 14:22:39 -0700

    Can't seem to toggle the form. Trying to display it in a new page

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,23 @@
 class RatingsController < ApplicationController
+	def new
+		redirect_to orders_path, notice: "This Order is already rated." if Order.find(params[:order_id]).is_rated
+		@rating = Rating.new
+	end
+
+	def create
+		@rating = Rating.new(rating_params)
+
+		if @rating.save
+			@rating.order.is_rated = true
+			redirect_to orders_path, notice: "Order was successfully rated."
+		else
+			redirect_to orders_path, notice: "Something Went Wrong!"
+		end
+	end
+
+	private
+
+		def rating_params
+			params.require(:rating).permit(:stars, :comment, :order_id)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'application#hello'
   get '/orders', to: 'orders#index'
+  get '/ratings', to: 'ratings#new'
+  post '/ratings', to: 'ratings#create'
 end
