3ec7110a9298d45efe8ce21def6d4b741be64694
navyabattula <navyabio12@gmail.com>
2021-11-04 10:00:26 +0530

    Changed the redirection route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,9 +11,9 @@ class ItemsController < ApplicationController
 		@item = Item.new(item_params)
 		
 		if @item.save
-			redirect_to items_new_path, notice: "You Have Successfully posted an Item!"
+			redirect_to root_path, notice: "You Have Successfully posted an Item!"
 		else
-			redirect_to items_new_path, notice: "Something went wrong. Try again. Item not posted!"
+			redirect_to root_path, notice: "Something went wrong. Try again. Item not posted!"
 		end	
 	end
 	private 
