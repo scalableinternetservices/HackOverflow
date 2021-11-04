5ca0d22b7b84f7467384e1c9ecc956ea8ec85a42
sharathvemula <svemula@ucsb.edu>
2021-11-03 21:55:35 -0700

    Updated address.rb

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,5 +3,5 @@ class Address < ApplicationRecord
 	validates :city, presence: true, length: {maximum: 50}
 	validates :state, presence: true, length: {maximum: 50}
 	VALID_ZIPCODE_REGEX = /\A[0-9]{5}(?:-[0-9]{4})?\z/i
-	validates :zipcode, presence: true, format: { with: VALID_EMAIL_REGEX }
+	validates :zipcode, presence: true, format: { with: VALID_ZIPCODE_REGEX }
 end
