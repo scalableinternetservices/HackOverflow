ce5b5390267fcb139ce7346fe8cf9659bd29fb19
saikumarysk <y.saikumar1997@gmail.com>
2021-11-07 16:47:30 -0800

    Added Ratings to orders and stars too

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,34 @@
 class ItemsController < ApplicationController
 	def new
-		@item = Item.new	
+		@item = Item.new
 	end
 
 	def show
 		@item = Item.find(params[:id])
+		@orders = @item.orders
+		@ratings = []
+		@stars = []
+		@orders.each do |order|
+			if !!(order.rating)
+				@ratings << order.rating
+				@stars << order.rating.stars
+			end
+		end
+		@overall_rating = @stars.sum / @stars.count
 	end
-    
+
 	def create
 		@item = Item.new(item_params)
-		
+
 		if @item.save
 			redirect_to root_path, notice: "You Have Successfully posted an Item!"
 		else
 			redirect_to root_path, notice: "Something went wrong. Try again. Item not posted!"
-		end	
+		end
 	end
-	private 
+	private
 		def item_params
 			params.require(:item).permit(:name, :description, :price)
-		end	
-		
+		end
+
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,9 @@ class Rating < ApplicationRecord
 	belongs_to :order
 	validates :stars, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 	validates :comment, presence: true, length: { maximum: 250 }
+
+	# Helper functions
+	def blank_stars
+		return 5 - self.stars
+	end
 end
