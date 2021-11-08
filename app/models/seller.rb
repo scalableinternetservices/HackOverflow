9254926e9395914ff740ddd522d4926c981e55dc
saikumarysk <y.saikumar1997@gmail.com>
2021-11-07 18:56:33 -0800

    Destroyed Seller Model

--- app/models/seller.rb
+++
@@ -1,7 +0,0 @@
-class Seller < ApplicationRecord
-	before_save { self.email.downcase! }
-	validates :firstname, presence: true, length: {maximum: 32}
-	validates :lastname, presence: true, length: {maximum: 32}
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
-end
