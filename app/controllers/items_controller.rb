cc2700de4f74e0a1273458803f519d66ddcaab4a
saikumarysk <y.saikumar1997@gmail.com>
2021-11-08 13:15:58 -0800

    Added authentication requirements

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+	before_action :authenticate_seller!, only: [:create]
+
 	def new
 		@item = Item.new
 	end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+	before_action :authenticate_buyer!
 
 	def index
 		@orders = Order.all.reverse # Needed to show latest orders first
