7757b8c5a8277c76a65d8b5d2fce7e698ec3a16d
Arjun <91302343+arjun-vb@users.noreply.github.com>
2021-11-11 11:16:37 -0800

    Updated seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times do |x|
-    Item.create!(name: x, description: x+10, price: x+20)
-end
-
-4.times do |x|
-    Cart.create!(buyer_id:1, item_id: x+1)
-end
-
-4.times do |x|
-    Cart.create!(buyer_id:0, item_id: x+1)
-end
-
-
-Buyer.create!(email: "0@gmail.com", encrypted_password: "password", password: "password")
-Buyer.create!(email: "1@gmail.com", encrypted_password: "password", password: "password")
-
-
-3.times do |x|
-    Address.create!(buyer_id: 1, street_address: "street", city: "city", state: "state", zipcode: "93106")
-end
