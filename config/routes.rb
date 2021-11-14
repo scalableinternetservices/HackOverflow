7fc040a32255b4ec0c10803f8cea0dace14e2e2b
arjun-vb <vbarjun1009@gmail.com>
2021-11-13 19:07:21 -0800

    added notices and alerts

--- config/routes.rb
+++ config/routes.rb
@@ -6,19 +6,23 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
   get '/orders', to: 'orders#index'
+
+  # Ratinds controller
   get '/ratings/:order_id', to: 'ratings#new', as: 'new_rating'
-  get '/items/new', to: 'items#new'
-  post '/items', to: 'items#create'
   post '/ratings/:order_id', to: 'ratings#create'
   
-  #Cart controller
+  # Cart controller
   get '/showcart', to: 'cart#showcart'
-  post '/orderplaced', to: 'cart#placeorder'
-  
-  # route to get item id info
-  get '/items/:id', to: 'items#show'
   post '/cart/:id', to: 'cart#additem'
   delete '/cart/:id', to: 'cart#deleteitem'
+  post '/orderplaced', to: 'cart#placeorder'
+  
+  # Items controller
+  get '/items/:id', to: 'items#show'
+  get '/items/new', to: 'items#new'
+  post '/items', to: 'items#create'
+  
+  # Profile controller
   get '/profile/buyer', to: 'profiles#buyer'
   get '/profile/seller', to: 'profiles#seller'  
 
