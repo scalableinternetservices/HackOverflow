1bbee8f919aa3000bd331c772c8eb28c97ff82a3
navyabattula <navyabio12@gmail.com>
2021-11-18 06:54:08 +0530

    changes in local system

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_11_060757) do
+ActiveRecord::Schema.define(version: 2021_11_11_050155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "buyer_id"
+  end
 
   create_table "buyers", force: :cascade do |t|
     t.string "email", default: "", null: false
