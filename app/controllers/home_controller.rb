bbae83604ad799733056be465081bd2d85ada2da
saikumarysk <y.saikumar1997@gmail.com>
2021-11-21 17:51:29 -0800

    Few Changes. Pagination working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @items = Item.paginate(page: params[:page])
+    @items = Item.paginate(page: params[:page], per_page: 10) # Can't reverse. No need I guess
   end
 end
