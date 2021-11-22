afdff6ede0808268ea115d816981edc01902e5dc
saikumarysk <y.saikumar1997@gmail.com>
2021-11-21 17:44:11 -0800

    Added Pagination

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @items = Item.all.reverse # Needed to show recently added items first
+    @items = Item.paginate(page: params[:page])
   end
 end
